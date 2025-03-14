13334b1e7f369729b91429449d1a0b8f1b45abbf
S Naren Kumar <vssnaren@gmail.com>
2025-03-15 00:26:31 +0530

    try1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Set a default value to prevent "undefined" error
+const basePath = import.meta.env.VITE_BASE_PATH || "/html-components-to-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "HTML-Components-To-Reactcopm-client-",
+  base: basePath,
 });
